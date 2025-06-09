53a4cb2c739ca692f72e73b7c2c94fa09bf3ade9
ayushudaydubey <102001ayushdubey@gmail.com>
2025-06-09 14:21:48 +0530

    connect frontend

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,7 @@ const server = createServer(app)
 // Initialize Socket.IO FIRST
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173','https://chat-app-react-neon-nine.vercel.app'],
+    origin: ['http://localhost:5173','https://chatzone-pi.vercel.app'],
     methods: ["GET", "POST"],
     credentials: true
   }
