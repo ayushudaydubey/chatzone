2f3c6fa5a0ed5a657935171f40afc25b79c2b65d
ayushudaydubey <102001ayushdubey@gmail.com>
2025-06-09 16:14:22 +0530

    something

--- Backend/src/utils/multer.js
+++
@@ -1,9 +0,0 @@
-// multer.js
-import multer from "multer";
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-const multerUpload = upload.single('file');
-
-export default multerUpload;
